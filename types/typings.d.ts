e49e84346b24f7255f95fd7a20a7e2a862a719a5
Austin <amcdaniel2@gmail.com>
2022-05-02 13:20:57 -0400

    remove dupe entry

--- types/typings.d.ts
+++ types/typings.d.ts
@@ -2,22 +2,3 @@ declare module '*.json';
 declare module '*.css';
 declare module '*.scss';
 declare module '*.md';
-
-declare global {
-  import { ReactThreeFiber } from 'react-three-fiber';
-  import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-  import CameraControls from 'camera-controls';
-
-  namespace JSX {
-    interface IntrinsicElements {
-      orbitControls: ReactThreeFiber.Object3DNode<
-        OrbitControls,
-        typeof OrbitControls
-      >;
-      cameraControls: ReactThreeFiber.Object3DNode<
-        CameraControls,
-        typeof CameraControls
-      >;
-    }
-  }
-}
