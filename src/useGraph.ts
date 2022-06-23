6e678a34c792f6e2b2b0ca78e6ac8a33fad348ee
Austin <amcdaniel2@gmail.com>
2022-06-23 08:18:40 -0400

    abstract builder

--- src/useGraph.ts
+++ src/useGraph.ts
@@ -1,8 +1,8 @@
 import { useRef, useState, useCallback, useEffect } from 'react';
-import { nodeSizeProvider, SizingType } from './sizing';
+import { SizingType } from './sizing';
 import { LayoutTypes, layoutProvider, LayoutStrategy } from './layout';
 import ngraph from 'ngraph.graph';
-import { calcLabelVisibility, LabelVisibilityType } from './utils/visibility';
+import { LabelVisibilityType } from './utils/visibility';
 import { tick } from './layout/layoutUtils';
 import {
   GraphEdge,
@@ -10,6 +10,7 @@ import {
   InternalGraphEdge,
   InternalGraphNode
 } from './types';
+import { buildGraph, transformGraph } from './utils/buildGraph';
 
 export interface GraphInputs {
   nodes: GraphNode[];
@@ -90,104 +91,3 @@ export const useGraph = ({
     edges: internalEdges
   };
 };
-
-function buildGraph(graph, nodes: GraphNode[], edges: GraphEdge[]) {
-  graph.clear();
-  graph.beginUpdate();
-
-  for (const node of nodes) {
-    graph.addNode(node.id, node);
-  }
-
-  for (const edge of edges) {
-    graph.addLink(edge.source, edge.target, edge);
-  }
-
-  graph.endUpdate();
-}
-
-interface TransformGraphInput {
-  graph: any;
-  layout: any;
-  sizingType?: SizingType;
-  labelType?: LabelVisibilityType;
-  sizingAttribute?: string;
-}
-
-function transformGraph({
-  graph,
-  layout,
-  sizingType,
-  labelType,
-  sizingAttribute
-}: TransformGraphInput) {
-  const nodes = [];
-  const edges = [];
-  const map = new Map();
-
-  const sizes = nodeSizeProvider(graph, sizingType, sizingAttribute);
-  const nodeCount = graph.getNodesCount();
-  const checkVisibility = calcLabelVisibility(nodeCount, labelType);
-
-  graph.forEachNode((node: InternalGraphNode) => {
-    if (node.data) {
-      const position = layout.getNodePosition(node.id);
-      const { data, fill, icon, label, size, ...rest } = node.data;
-      const nodeSize = sizes.getSizeForNode(node.id, size);
-      const labelVisible = checkVisibility('node', nodeSize);
-
-      const n = {
-        ...node,
-        size: nodeSize,
-        labelVisible,
-        label,
-        icon,
-        fill,
-        data: {
-          ...rest,
-          ...(data || {})
-        },
-        position: {
-          ...position,
-          z: position.z || 1
-        }
-      };
-
-      map.set(node.id, n);
-      nodes.push(n);
-    }
-  });
-
-  graph.forEachLink((link: InternalGraphEdge) => {
-    const from = map.get(link.fromId);
-    const to = map.get(link.toId);
-
-    if (from && to) {
-      const { data, id, label, size, ...rest } = link.data;
-      const labelVisible = checkVisibility('edge', link.size);
-
-      edges.push({
-        ...link,
-        id,
-        label,
-        labelVisible,
-        size,
-        from,
-        to,
-        data: {
-          ...rest,
-          ...(data || {})
-        },
-        position: {
-          from: from.position,
-          to: to.position
-        }
-      });
-    }
-  });
-
-  return {
-    nodes,
-    edges
-  };
-}
---
+++ src/utils/buildGraph.ts
@@ -0,0 +1,109 @@
+import { nodeSizeProvider, SizingType } from '../sizing';
+import {
+  GraphEdge,
+  GraphNode,
+  InternalGraphEdge,
+  InternalGraphNode
+} from '../types';
+import { calcLabelVisibility, LabelVisibilityType } from './visibility';
+
+export function buildGraph(graph, nodes: GraphNode[], edges: GraphEdge[]) {
+  graph.clear();
+  graph.beginUpdate();
+
+  for (const node of nodes) {
+    graph.addNode(node.id, node);
+  }
+
+  for (const edge of edges) {
+    graph.addLink(edge.source, edge.target, edge);
+  }
+
+  graph.endUpdate();
+}
+
+interface TransformGraphInput {
+  graph: any;
+  layout: any;
+  sizingType?: SizingType;
+  labelType?: LabelVisibilityType;
+  sizingAttribute?: string;
+}
+
+export function transformGraph({
+  graph,
+  layout,
+  sizingType,
+  labelType,
+  sizingAttribute
+}: TransformGraphInput) {
+  const nodes = [];
+  const edges = [];
+  const map = new Map();
+
+  const sizes = nodeSizeProvider(graph, sizingType, sizingAttribute);
+  const nodeCount = graph.getNodesCount();
+  const checkVisibility = calcLabelVisibility(nodeCount, labelType);
+
+  graph.forEachNode((node: InternalGraphNode) => {
+    if (node.data) {
+      const position = layout.getNodePosition(node.id);
+      const { data, fill, icon, label, size, ...rest } = node.data;
+      const nodeSize = sizes.getSizeForNode(node.id, size);
+      const labelVisible = checkVisibility('node', nodeSize);
+
+      const n = {
+        ...node,
+        size: nodeSize,
+        labelVisible,
+        label,
+        icon,
+        fill,
+        data: {
+          ...rest,
+          ...(data || {})
+        },
+        position: {
+          ...position,
+          z: position.z || 1
+        }
+      };
+
+      map.set(node.id, n);
+      nodes.push(n);
+    }
+  });
+
+  graph.forEachLink((link: InternalGraphEdge) => {
+    const from = map.get(link.fromId);
+    const to = map.get(link.toId);
+
+    if (from && to) {
+      const { data, id, label, size, ...rest } = link.data;
+      const labelVisible = checkVisibility('edge', link.size);
+
+      edges.push({
+        ...link,
+        id,
+        label,
+        labelVisible,
+        size,
+        from,
+        to,
+        data: {
+          ...rest,
+          ...(data || {})
+        },
+        position: {
+          from: from.position,
+          to: to.position
+        }
+      });
+    }
+  });
+
+  return {
+    nodes,
+    edges
+  };
+}
