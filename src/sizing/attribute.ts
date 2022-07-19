515ce93f50789f8edd56cf23e5fd64f65076dc2a
Austin <amcdaniel2@gmail.com>
2022-07-19 09:51:36 -0400

    fix attribute missing error

--- src/sizing/attribute.ts
+++ src/sizing/attribute.ts
@@ -5,19 +5,20 @@ export function attributeSizing({
   attribute,
   defaultSize
 }: SizingStrategyInputs): SizingStrategy {
-  if (!attribute) {
+  const map = new Map();
+
+  if (attribute) {
+    graph.forEachNode(node => {
+      const size = node.data.data[attribute];
+      if (isNaN(size)) {
+        console.warn(`Attribute ${size} is not a number for node ${node.id}`);
+      }
+
+      map.set(node.id, size || 0);
+    });
+  } else {
     console.warn('Attribute sizing configured but no attribute provided');
   }
-
-  const map = new Map();
-  graph.forEachNode(node => {
-    const size = node.data.data[attribute];
-    if (isNaN(size)) {
-      console.warn(`Attribute ${size} is not a number for node ${node.id}`);
-    }
-
-    map.set(node.id, size || 0);
-  });
 
   return {
     getSizeForNode: (nodeId: string) => {
