adfce7bc66ab52e890b864a2fcc0b3663d69a46a
Austin <amcdaniel2@gmail.com>
2022-08-17 13:03:31 -0400

    perf improvement

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -2,5 +2,6 @@ export const animationConfig = {
   mass: 10,
   tension: 1000,
   friction: 300,
-  precision: 0.00001
+  // Decreasing precision to improve performance from 0.00001
+  precision: 0.1
 };
