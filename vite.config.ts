be91301f162921f8ff89d6de6aa35f3400bb2857
amcdnl <amcdaniel2@gmail.com>
2023-05-09 10:27:44 -0400

    remove unused

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
