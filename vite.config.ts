1a719c27f6d030c16136962d2aa6097c7ddfc743
amcdnl <amcdaniel2@gmail.com>
2023-05-09 10:27:34 -0400

    fix deploy?

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -10,38 +10,55 @@ import external from 'rollup-plugin-peer-deps-external';
 import dts from 'vite-plugin-dts';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
-export default defineConfig({
-  plugins: [
-    svgrPlugin(),
-    tsconfigPaths(),
-    cssInjectedByJsPlugin(),
-    react(),
-    dts({
-      insertTypesEntry: true,
-      include: ['src']
-    }),
-    checker({
-      typescript: true
-    })
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  },
-  build: {
-    minify: false,
-    sourcemap: true,
-    lib: {
-      entry: resolve('src', 'index.ts'),
-      name: 'reagraph',
-      fileName: 'reagraph'
-    },
-    rollupOptions: {
+export default defineConfig(({ mode }) =>
+  mode === 'library'
+    ? {
       plugins: [
-        external({
-          includeDependencies: true
+        svgrPlugin(),
+        tsconfigPaths(),
+        cssInjectedByJsPlugin(),
+        react(),
+        dts({
+          insertTypesEntry: true,
+          include: ['src']
+        }),
+        checker({
+          typescript: true
         })
-      ]
+      ],
+      test: {
+        globals: true,
+        environment: 'jsdom'
+      },
+      build: {
+        minify: false,
+        sourcemap: true,
+        lib: {
+          entry: resolve('src', 'index.ts'),
+          name: 'reagraph',
+          fileName: 'reagraph'
+        },
+        rollupOptions: {
+          plugins: [
+            external({
+              includeDependencies: true
+            })
+          ]
+        }
+      }
     }
-  }
-});
+    : {
+      plugins: [
+        svgrPlugin(),
+        tsconfigPaths(),
+        react(),
+        checker({
+          typescript: true
+        })
+      ],
+      test: {
+        globals: true,
+        environment: 'jsdom'
+      }
+    }
+);
