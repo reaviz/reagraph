ba53092c285a7780cb549fdcf96aa7d544c8c945
amcdnl <amcdaniel2@gmail.com>
2024-02-19 07:25:06 -0500

    improve docs

--- src/CameraControls/useCameraControls.ts
+++ src/CameraControls/useCameraControls.ts
@@ -2,13 +2,45 @@ import CameraControls from 'camera-controls';
 import { createContext, useContext } from 'react';
 
 export interface CameraControlsContextProps {
+  /**
+   * The camera controls object.
+   */
   controls: CameraControls | null;
+
+  /**
+   * A function that resets the camera controls.
+   * If the optional `animated` argument is true, the reset is animated.
+   */
   resetControls: (animated?: boolean) => void;
+
+  /**
+   * A function that zooms in the camera.
+   */
   zoomIn: () => void;
+
+  /**
+   * A function that zooms out the camera.
+   */
   zoomOut: () => void;
+
+  /**
+   * A function that pans the camera to the left.
+   */
   panLeft: () => void;
+
+  /**
+   * A function that pans the camera to the right.
+   */
   panRight: () => void;
+
+  /**
+   * A function that pans the camera upwards.
+   */
   panUp: () => void;
+
+  /**
+   * A function that pans the camera downwards.
+   */
   panDown: () => void;
 }
 
