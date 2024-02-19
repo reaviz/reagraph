04967b8d0432e82148a0ab93124668fdfa652b87
amcdnl <amcdaniel2@gmail.com>
2024-02-19 07:24:17 -0500

    improve docs

--- src/RadialMenu/RadialMenu.tsx
+++ src/RadialMenu/RadialMenu.tsx
@@ -5,11 +5,35 @@ import css from './RadialMenu.module.css';
 import classNames from 'classnames';
 
 interface RadialMenuProps {
+  /**
+   * An array of menu items to be displayed in the radial menu.
+   */
   items: MenuItem[];
+
+  /**
+   * The radius of the radial menu.
+   */
   radius?: number;
+
+  /**
+   * The inner radius of the radial menu.
+   */
   innerRadius?: number;
+
+  /**
+   * The starting offset angle for the first menu item.
+   */
   startOffsetAngle?: number;
+
+  /**
+   * The CSS class name for the radial menu.
+   */
   className?: string;
+
+  /**
+   * A function that is called when the radial menu is closed.
+   * The function receives the mouse event that triggered the closure.
+   */
   onClose?: (event: React.MouseEvent<HTMLDivElement>) => void;
 }
 
