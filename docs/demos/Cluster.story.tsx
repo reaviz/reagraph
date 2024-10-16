85c54fc533a4e4faa433a80396344669a617922f
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2024-10-16 17:36:32 +0300

    sample to repro bug

--- docs/demos/Cluster.story.tsx
+++ docs/demos/Cluster.story.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { GraphCanvas, lightTheme } from '../../src';
-import { clusterNodes, clusterEdges, random, singleNodeClusterNodes, imbalancedClusterNodes, manyClusterNodes } from '../assets/demo';
+import {
+  clusterNodes,
+  clusterEdges,
+  random,
+  singleNodeClusterNodes,
+  imbalancedClusterNodes,
+  manyClusterNodes
+} from '../assets/demo';
 
 export default {
   title: 'Demos/Cluster',
   component: GraphCanvas
 };
 
-export const Simple = () => (
-  <GraphCanvas nodes={clusterNodes} draggable edges={[]} clusterAttribute="type" />
-);
+export const Simple = () => {
+  const [nodes, setNodes] = useState(clusterNodes);
+
+  const addNode = useCallback(() => {
+    const next = nodes.length + 2;
+    setNodes(prev => [
+      ...prev,
+      {
+        id: `${next}`,
+        label: `Node ${next}`,
+        fill: '#3730a3',
+        data: {
+          type: 'IP',
+          segment: next % 2 === 0 ? 'A' : undefined
+        }
+      }
+    ]);
+  }, [nodes]);
+
+  return (
+    <>
+      <GraphCanvas nodes={nodes} draggable edges={[]} clusterAttribute="type" />
+      <div style={{ zIndex: 9, position: 'absolute', top: 15, right: 15 }}>
+        <button type="button" onClick={addNode}>
+          Add node
+        </button>
+      </div>
+    </>
+  );
+};
 
 const clusterNodesWithSizes = clusterNodes.map(node => ({
   ...node,
@@ -17,27 +51,57 @@ const clusterNodesWithSizes = clusterNodes.map(node => ({
 }));
 
 export const Sizes = () => (
-  <GraphCanvas nodes={clusterNodesWithSizes} draggable edges={[]} clusterAttribute="type" />
+  <GraphCanvas
+    nodes={clusterNodesWithSizes}
+    draggable
+    edges={[]}
+    clusterAttribute="type"
+  />
 );
 
 export const SingleNodeClusters = () => (
-  <GraphCanvas nodes={singleNodeClusterNodes} draggable edges={[]} clusterAttribute="type" />
+  <GraphCanvas
+    nodes={singleNodeClusterNodes}
+    draggable
+    edges={[]}
+    clusterAttribute="type"
+  />
 );
 
 export const ImbalancedClusters = () => (
-  <GraphCanvas nodes={imbalancedClusterNodes} draggable edges={[]} clusterAttribute="type" />
+  <GraphCanvas
+    nodes={imbalancedClusterNodes}
+    draggable
+    edges={[]}
+    clusterAttribute="type"
+  />
 );
 
 export const LargeDataset = () => (
-  <GraphCanvas nodes={manyClusterNodes} draggable edges={[]} clusterAttribute="type" />
+  <GraphCanvas
+    nodes={manyClusterNodes}
+    draggable
+    edges={[]}
+    clusterAttribute="type"
+  />
 );
 
 export const Edges = () => (
-  <GraphCanvas nodes={clusterNodes} draggable edges={clusterEdges} clusterAttribute="type" />
+  <GraphCanvas
+    nodes={clusterNodes}
+    draggable
+    edges={clusterEdges}
+    clusterAttribute="type"
+  />
 );
 
 export const Selections = () => (
-  <GraphCanvas nodes={clusterNodes} selections={[clusterNodes[0].id]} edges={clusterEdges} clusterAttribute="type" />
+  <GraphCanvas
+    nodes={clusterNodes}
+    selections={[clusterNodes[0].id]}
+    edges={clusterEdges}
+    clusterAttribute="type"
+  />
 );
 
 export const Events = () => (
@@ -47,7 +111,9 @@ export const Events = () => (
     edges={clusterEdges}
     clusterAttribute="type"
     onClusterPointerOut={cluster => console.log('cluster pointer out', cluster)}
-    onClusterPointerOver={cluster => console.log('cluster pointer over', cluster)}
+    onClusterPointerOver={cluster =>
+      console.log('cluster pointer over', cluster)
+    }
     onClusterClick={cluster => console.log('cluster click', cluster)}
   />
 );
@@ -100,7 +166,8 @@ export const LabelsOnly = () => (
 export const ThreeDimensions = () => (
   <GraphCanvas
     nodes={clusterNodesWithSizes}
-    draggable edges={[]}
+    draggable
+    edges={[]}
     layoutType="forceDirected3d"
     clusterAttribute="type"
   >
@@ -108,7 +175,11 @@ export const ThreeDimensions = () => (
   </GraphCanvas>
 );
 
-
 export const Partial = () => (
-  <GraphCanvas nodes={clusterNodes} draggable edges={[]} clusterAttribute="segment" />
+  <GraphCanvas
+    nodes={clusterNodes}
+    draggable
+    edges={[]}
+    clusterAttribute="segment"
+  />
 );
