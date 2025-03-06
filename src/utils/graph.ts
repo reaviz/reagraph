b1c16a3fe518e2405d36b67473ecfbb0fa173cd6
Tyler Telis <xlitersps@gmail.com>
2025-03-06 10:44:54 -0500

    bugfix: Validation on adding nodes and edges when building the graph

--- src/utils/graph.ts
+++ src/utils/graph.ts
@@ -21,19 +21,28 @@ export function buildGraph(
   // smarter and only add/remove nodes
   graph.clear();
 
+  const addedNodes = new Set<string>();
+
   for (const node of nodes) {
     try {
-      graph.addNode(node.id, node);
+      if (!addedNodes.has(node.id)) {
+        graph.addNode(node.id, node);
+        addedNodes.add(node.id);
+      }
     } catch ({ message }) {
-      console.error(`[Graph] ${message}`);
+      console.error(`[Graph] Error adding node '${node.id}': ${message}`);
     }
   }
 
   for (const edge of edges) {
+    if (!addedNodes.has(edge.source) || !addedNodes.has(edge.target)) {
+      continue;
+    }
+
     try {
       graph.addEdge(edge.source, edge.target, edge);
     } catch ({ message }) {
-      console.error(`[Graph] ${message}`);
+      console.error(`[Graph] Error adding edge '${edge.source} -> ${edge.target}': ${message}`);
     }
   }
 
