63422591afae177fd664c094ef3e992484f68f88
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2025-05-30 15:10:11 +0300

    add doc block for utility method

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,3 +1,8 @@
+/**
+ * Check if an element is not editable (input, select, textarea, contentEditable).
+ * @param element - The element to check
+ * @returns True if the element is not editable, false otherwise
+ */
 export const isNotEditableElement = (element: HTMLElement) => {
   return (
     element.tagName !== 'INPUT' &&
