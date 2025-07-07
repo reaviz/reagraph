5c6627ebe06d3c968b43d2f15d31e4642f89eeb6
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2025-07-07 09:43:02 +0300

    rename folder

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: [
-    '../docs/**/*.story.tsx'
+    '../stories/**/*.story.tsx'
   ],
 
   addons: [
